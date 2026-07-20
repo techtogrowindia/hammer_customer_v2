55d8b43eee6c88f24c88e3e96ad58ff9c555472d
Aswick Jothi <aswick.jothi@staples.com>
2026-07-20 17:15:11 +0530

    FIX

--- src/domain/models/orders/place-order-response.ts
+++ src/domain/models/orders/place-order-response.ts
@@ -10,6 +10,7 @@ export interface GPDData {
   order_id?: number;
   order_number?: string;
   status?: string;
+  can_cancel?: boolean;
   service?: Service;
   issue_description?: string;
   type?: string;
@@ -20,16 +21,45 @@ export interface GPDData {
   voice_notes?: string[];
   assigned_technicians_count?: number;
   quotes_submitted_count?: number;
+  rejections_count?: number;
   technician_quotes?: TechnicianQuote[];
+  technician_rejections?: any[];
+  confirmed_technician?: null;
+  additional_quotes?: any[];
+  invoice?: Invoice;
+  status_log?: StatusLog[];
   amount_rupees?: null;
   tax_amount_rupees?: null;
   created_at?: Date;
   updated_at?: Date;
 }
 
+export interface Invoice {
+  initial_quote?: InitialQuote;
+  additional_quotes?: any[];
+  additional_quotes_total?: number;
+  total_amount_rupees?: null;
+}
+
+export interface InitialQuote {
+  desc?: string;
+  amount?: null;
+}
+
 export interface Service {
   id?: number;
   service_name?: string;
+}
+
+export interface StatusLog {
+  event?: string;
+  status?: string;
+  previous_status?: null;
+  title?: string;
+  message?: string;
+  actor_type?: string;
+  metadata?: any[];
+  occurred_at?: Date;
 }
 
 export interface TechnicianQuote {
